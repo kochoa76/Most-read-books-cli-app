ae64f3346cb5e2441719268b4dbb97f88ba6fa42
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-19 17:45:31 +0000

    methods CLI

--- lib/most_read_books/cli.rb
+++ lib/most_read_books/cli.rb
@@ -2,6 +2,16 @@ class MostReadBooks::CLI
 
   def call
     puts "Most Read Books This Week:"
+    list_deals
   end
 
-end 
+  def list_deals
+    puts <<-DOC.gsub /^\s*/, ''
+    1. "The Great Alone" - by "Kristin Hannah" - 4.35/ 5 rating - ready by: 3,407 people this week
+    2. "Little Fires Everywhere" - by "Celeste Ng" - 4.16/5 rating - read by: 2,694 people this week
+  DOC
+  end
+
+
+
+end
