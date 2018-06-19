401b1e8a6a1ca933d3da6b4c6590fb0d73470016
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-19 20:21:20 +0000

    update

--- lib/most_read_books/book.rb
+++ lib/most_read_books/book.rb
@@ -3,10 +3,10 @@ class MostReadBooks::Book
   def self.weeks_books
     #should return instances of books
     puts <<-DOC.gsub /^\s*/, ''
-    1. "The Great Alone" - by "Kristin Hannah" - 4.35/ 5 rating - ready by: 3,407 people this week
+    1. "The Great Alone" - by "Kristin Hannah" - 4.35/ 5 rating - read by: 3,407 people this week
     2. "Little Fires Everywhere" - by "Celeste Ng" - 4.16/5 rating - read by: 2,694 people this week
   DOC
-  
+
 end
 
 end
