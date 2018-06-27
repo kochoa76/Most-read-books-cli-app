03a0b7886016ccb558d2adcf1ab0f9ef715b55a0
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-27 19:26:17 +0000

    removed bookscraper

--- lib/most_read_books.rb
+++ lib/most_read_books.rb
@@ -4,4 +4,3 @@ require 'pry'
 require_relative "./most_read_books/version"
 require_relative "./most_read_books/cli"
 require_relative "./most_read_books/book"
-require_relative "./most_read_books/book_scraper"
